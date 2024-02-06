cda76abf5eb7a3f396c3252655d42be87aced4ef
Amisha <amisha.i@canopas.com>
2024-02-06 14:32:49 +0530

    Initial Commit

---
+++ Splito/SplitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitoApp.swift
+//  Splito
+//
+//  Created by Amisha Italiya on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
