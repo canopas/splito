fd46d85621c00b3693dc359ab6c3b1a88baaf57f
Amisha <amisha.i@canopas.com>
2024-02-12 17:12:35 +0530

    Setup project

---
+++ Data/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Amisha Italiya on 12/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
---
+++ UI/UI/UI.h
@@ -0,0 +1,18 @@
+//
+//  UI.h
+//  UI
+//
+//  Created by Amisha Italiya on 12/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UI.
+FOUNDATION_EXPORT double UIVersionNumber;
+
+//! Project version string for UI.
+FOUNDATION_EXPORT const unsigned char UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UI/PublicHeader.h>
+
+
