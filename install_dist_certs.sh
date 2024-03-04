ba50eedd9f4602f96c07c57dbe5ceaf1e0716e2d
Amisha <amisha.i@canopas.com>
2024-03-04 12:42:47 +0530

    Change script

--- install_dist_certs.sh
+++ install_dist_certs.sh
@@ -6,21 +6,23 @@ CERTIFICATE_P12=dist_certificate.p12
 # Recreate the certificate from the secure environment variable
 echo $BUILD_CERTIFICATE_KEY | base64 --decode > $CERTIFICATE_P12
 
-# Lock the keychain
-#security lock-keychain $BUILD_KEYCHAIN
-
-# Unlock the keychain
-#security unlock-keychain -p $BUILD_KEYCHAIN_PASSWORD $BUILD_KEYCHAIN
-
+echo "XXX --- BUILD_KEYCHAIN_PASSWORD: $BUILD_KEYCHAIN_PASSWORD"
 # Set keychain settings
 security set-keychain-settings $BUILD_KEYCHAIN
 
+echo "XXX --- Unlocking keychain..."
+# Unlock the keychain
+security unlock-keychain -p $BUILD_KEYCHAIN_PASSWORD $BUILD_KEYCHAIN
+
+echo "XXX --- Import the certificate to the keychain"
 # Import the certificate to the keychain
 security import $CERTIFICATE_P12 -k $BUILD_KEYCHAIN -P $BUILD_CERTIFICATE_PASSWORD -T /usr/bin/codesign;
 
+echo "XXX --- Set key partition list"
 # Set key partition list
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $BUILD_KEYCHAIN_PASSWORD $BUILD_KEYCHAIN
 
+echo "XXX --- Lock the keychain"
 # Lock the keychain
 security lock-keychain $BUILD_KEYCHAIN
 
