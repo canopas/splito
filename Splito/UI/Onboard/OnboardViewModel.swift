88bfdd1810258d6ec0ede60acfce9cb1b74e0051
NiraliSonani <nirali.s@canopas.com>
2024-05-27 18:16:53 +0530

    Fixed pipeline

--- Splito/UI/Onboard/OnboardViewModel.swift
+++ Splito/UI/Onboard/OnboardViewModel.swift
@@ -11,32 +11,36 @@ import FirebaseAuth
 import Foundation
 
 class OnboardViewModel: BaseViewModel, ObservableObject {
-
+    
     @Published var currentPageIndex = 0
     @Published private(set) var showLoader = false
-
+    
     @Inject private var preference: SplitoPreference
-
+    
     private let router: Router<AppRoute>
-
+    
     init(router: Router<AppRoute>) {
         self.router = router
     }
-
+    
     func loginAnonymous() {
         showLoader = true
-        FirebaseProvider.auth.signInAnonymously { [weak self] result, _ in
+        FirebaseProvider.auth.signInAnonymously { [weak self] result, error in
             guard let self = self else { return }
             if let error {
                 self.showLoader = false
                 self.alert = .init(message: "Server error")
                 self.showAlert = true
+            } else if let result {
+                self.preference.isOnboardShown = result.user.isAnonymous
+                self.showLoader = false
+                router.updateRoot(root: .LoginView)
+            } else {
+                self.alert = .init(message: "Contact Support")
+                self.showLoader = false
+                self.showAlert = true
             }
-
-            guard let user = result?.user else { self.showLoader = false; return }
-            self.preference.isOnboardShown = user.isAnonymous
-            self.showLoader = false
-            router.updateRoot(root: .LoginView)
+            
         }
     }
 }
