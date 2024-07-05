8737e184271d7065a57d3fada29b6a6c38c17259
NiraliSonani <nirali.s@canopas.com>
2024-07-05 19:48:59 +0530

    Refactor code

--- Splito/UI/Home/Expense/AddExpenseViewModel.swift
+++ Splito/UI/Home/Expense/AddExpenseViewModel.swift
@@ -226,14 +226,14 @@ extension AddExpenseViewModel {
             newExpense.date = Timestamp(date: expenseDate)
             newExpense.paidBy = selectedPayer.id
             newExpense.splitType = splitType
-            newExpense.splitTo = (splitType == .percentage || splitType == .shares) ? splitData.map({ $0.key }) : selectedMembers
+            newExpense.splitTo = (splitType == .equally) ? selectedMembers : splitData.map({ $0.key })
             newExpense.splitData = splitData
 
             updateExpense(expense: newExpense)
         } else {
             let expense = Expense(name: expenseName.trimming(spaces: .leadingAndTrailing).capitalized, amount: expenseAmount,
                                   date: Timestamp(date: expenseDate), paidBy: selectedPayer.id, addedBy: user.id,
-                                  splitTo: (splitType == .percentage || splitType == .shares) ? splitData.map({ $0.key }) : selectedMembers,
+                                  splitTo: (splitType == .equally) ? selectedMembers : splitData.map({ $0.key }),
                                   groupId: groupId, splitType: splitType, splitData: splitData)
 
             addExpense(expense: expense, completion: completion)
